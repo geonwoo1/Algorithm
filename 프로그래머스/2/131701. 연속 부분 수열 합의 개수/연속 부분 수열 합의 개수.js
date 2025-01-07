940828cb820d3eac4b751d0912ba6f1a8c4aa584
이건우 <90121680+geonwoo1@users.noreply.github.com>
2025-01-07 22:07:17 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 215.75 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -4,8 +4,9 @@ function solution(elements) {
     let circleElemets = elements.concat(elements);
     
     for(var i = 1; i <= n; i++){
+        let sum = 0;
         for(var j = 0; j < n; j++){
-            let sum = circleElemets.slice(j, j + i).reduce((a, b) => a + b, 0);
+            sum += circleElemets[i + j];
             set.add(sum);
         }
     }     
