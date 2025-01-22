96a7568697e971419948e1038a50dc6f75b06aa6
이건우 <90121680+geonwoo1@users.noreply.github.com>
2025-01-22 17:53:31 +0900

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -7,38 +7,74 @@ const problemsDir = path.join(__dirname, '프로그래머스');
 // README 파일 경로
 const readmePath = path.join(__dirname, 'README.md');
 
-// 문제 개수 제한 (상위 N개만 표시)
-const MAX_PROBLEMS_TO_SHOW = 10;
-
 // README 업데이트 함수
 const updateReadme = () => {
-    const problemFiles = fs.readdirSync(problemsDir);
-    const problemList = problemFiles.map(file => `- [${file.replace('.js', '')}](프로그래머스/${file})`);
-    
-    // 문제 개수 계산
-    const problemCount = problemList.length;
-    
-    // 상위 N개 문제만 표시
-    const displayedProblems = problemList.slice(0, MAX_PROBLEMS_TO_SHOW);
-    
-    // "더 보기" 링크 추가
-    const moreLink = problemCount > MAX_PROBLEMS_TO_SHOW ? `\n\n[더 보기](${readmePath})` : '';
-    
+    const difficultyCount = {
+        'LEVEL 0': 0,
+        'LEVEL 1': 0,
+        'LEVEL 2': 0,
+        'LEVEL 3': 0,
+        'LEVEL 4': 0,
+        'LEVEL 5': 0
+    };
+
+    const problemList = [];
+
+    // 프로그래머스 디렉토리 내의 난이도 폴더를 읽기
+    const difficultyFolders = fs.readdirSync(problemsDir);
+
+    difficultyFolders.forEach(folder => {
+        const folderPath = path.join(problemsDir, folder);
+        
+        // 폴더가 디렉토리인지 확인
+        if (fs.statSync(folderPath).isDirectory()) {
+            const problemFiles = fs.readdirSync(folderPath);
+            
+            problemFiles.forEach(file => {
+                const problemName = file.replace('.js', '');
+                problemList.push(`- [${problemName}](프로그래머스/${folder}/${file})`);
+
+                // 난이도 추출 (폴더 이름에서 난이도를 확인)
+                if (folder.includes('0')) {
+                    difficultyCount['LEVEL 0']++;
+                } else if (folder.includes('1')) {
+                    difficultyCount['LEVEL 1']++;
+                } else if (folder.includes('2')) {
+                    difficultyCount['LEVEL 2']++;
+                }else if (folder.includes('3')) {
+                    difficultyCount['LEVEL 3']++;
+                }else if (folder.includes('4')) {
+                    difficultyCount['LEVEL 4']++;
+                }else if (folder.includes('5')) {
+                    difficultyCount['LEVEL 5']++;
+                }
+            });
+        }
+    });
+
+    // 총 문제 개수 계산
+    const totalProblems = problemList.length;
+
     // README 내용 구성
     const readmeContent = `
 # 문제 목록
 
 ## 프로그래머스 문제
-현재까지 풀어본 문제는 **${problemCount}문제**입니다.
+현재까지 풀어본 문제는 **${totalProblems}문제**입니다.
+- LEVEL 0: ${difficultyCount['LEVEL 0']}문제
+- LEVEL 1: ${difficultyCount['LEVEL 1']}문제
+- LEVEL 2: ${difficultyCount['LEVEL 2']}문제
+- LEVEL 3: ${difficultyCount['LEVEL 3']}문제
+- LEVEL 4: ${difficultyCount['LEVEL 4']}문제
+- LEVEL 5: ${difficultyCount['LEVEL 5']}문제
+
 
 ### 문제 목록:
-${displayedProblems.join('\n')}
-
-${moreLink}
+${problemList.join('\n')}
 
 ---
 
-이 리스트는 자동으로 업데이트됩니다.
+이 리스트는 자동으로 업데이트됩니다. 마지막 업데이트: ${new Date().toLocaleString()}
     `;
 
     // README 파일에 내용 쓰기
