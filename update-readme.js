1084eb49ad59b0499428135d3415bdbfebf84b7a
이건우 <90121680+geonwoo1@users.noreply.github.com>
2025-01-22 17:58:01 +0900

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -12,13 +12,14 @@ const updateReadme = () => {
     const difficultyCount = {
         'LEVEL 0': 0,
         'LEVEL 1': 0,
-        'LEVEL 2': 0,
-        'LEVEL 3': 0,
-        'LEVEL 4': 0,
-        'LEVEL 5': 0
+        'LEVEL 2': 0
     };
 
-    const problemList = [];
+    const problemsByLevel = {
+        'LEVEL 0': [],
+        'LEVEL 1': [],
+        'LEVEL 2': []
+    };
 
     // 프로그래머스 디렉토리 내의 난이도 폴더를 읽기
     const difficultyFolders = fs.readdirSync(problemsDir);
@@ -32,28 +33,24 @@ const updateReadme = () => {
             
             problemFiles.forEach(file => {
                 const problemName = file.replace('.js', '');
-                problemList.push(`- [${problemName}](프로그래머스/${folder}/${file})`);
-
+                
                 // 난이도 추출 (폴더 이름에서 난이도를 확인)
                 if (folder.includes('0')) {
                     difficultyCount['LEVEL 0']++;
+                    problemsByLevel['LEVEL 0'].push(`- [${problemName}](프로그래머스/${folder}/${file})`);
                 } else if (folder.includes('1')) {
                     difficultyCount['LEVEL 1']++;
+                    problemsByLevel['LEVEL 1'].push(`- [${problemName}](프로그래머스/${folder}/${file})`);
                 } else if (folder.includes('2')) {
                     difficultyCount['LEVEL 2']++;
-                }else if (folder.includes('3')) {
-                    difficultyCount['LEVEL 3']++;
-                }else if (folder.includes('4')) {
-                    difficultyCount['LEVEL 4']++;
-                }else if (folder.includes('5')) {
-                    difficultyCount['LEVEL 5']++;
+                    problemsByLevel['LEVEL 2'].push(`- [${problemName}](프로그래머스/${folder}/${file})`);
                 }
             });
         }
     });
 
     // 총 문제 개수 계산
-    const totalProblems = problemList.length;
+    const totalProblems = difficultyCount['LEVEL 0'] + difficultyCount['LEVEL 1'] + difficultyCount['LEVEL 2'];
 
     // README 내용 구성
     const readmeContent = `
@@ -64,14 +61,23 @@ const updateReadme = () => {
 - LEVEL 0: ${difficultyCount['LEVEL 0']}문제
 - LEVEL 1: ${difficultyCount['LEVEL 1']}문제
 - LEVEL 2: ${difficultyCount['LEVEL 2']}문제
-- LEVEL 3: ${difficultyCount['LEVEL 3']}문제
-- LEVEL 4: ${difficultyCount['LEVEL 4']}문제
-- LEVEL 5: ${difficultyCount['LEVEL 5']}문제
-
 
 ### 문제 목록:
-${problemList.join('\n')}
 
+#### LEVEL 0 문제:
+${problemsByLevel['LEVEL 0'].join('\n')}
+
+#### LEVEL 1 문제:
+${problemsByLevel['LEVEL 1'].join('\n')}
+
+#### LEVEL 2 문제:
+${problemsByLevel['LEVEL 2'].join('\n')}
+
+#### LEVEL 3 문제:
+${problemsByLevel['LEVEL 3'].join('\n')}
+
+#### LEVEL 4 문제:
+${problemsByLevel['LEVEL 4'].join('\n')}
 ---
 
 이 리스트는 자동으로 업데이트됩니다. 마지막 업데이트: ${new Date().toLocaleString()}
