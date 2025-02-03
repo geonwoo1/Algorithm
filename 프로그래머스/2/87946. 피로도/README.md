aced9649612624c51e8fb13f7de5d09b06612096
이건우 <90121680+geonwoo1@users.noreply.github.com>
2025-02-03 20:25:36 +0900

    [level 2] Title: 피로도, Time: 8.89 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=javascript) 
 
 ### 성능 요약
 
-메모리: 73.9 MB, 시간: 4.44 ms
+메모리: 38.1 MB, 시간: 8.89 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 01월 24일 11:51:52
+2025년 02월 03일 20:25:33
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,29 +1,27 @@
 function solution(k, dungeons) {
-    let maxDungeons = 0;  // 최대 탐험할 수 있는 던전 수
 
-    // DFS (깊이 우선 탐색)을 사용하여 모든 던전 탐험 순서를 탐색
-    function dfs(currentFatigue, exploredCount, visited) {
-        // 현재까지 탐험한 던전 수가 최대치라면 업데이트
-        maxDungeons = Math.max(maxDungeons, exploredCount);
-
-        // 모든 던전을 탐험할 수 있는 경우 탐색
-        for (let i = 0; i < dungeons.length; i++) {
-            const [minRequired, fatigueCost] = dungeons[i];
-
-            // 이미 방문한 던전은 건너뜁니다
+    let dungeonsCnt = 0;
+    let visited = Array(dungeons.length).fill(false);
+    
+    function dfs(current, cnt){
+        dungeonsCnt = Math.max(dungeonsCnt, cnt);
+        for(var i = 0; i < dungeons.length; i++){
+            const [min, cost] = dungeons[i];
             if (visited[i]) continue;
-
-            // 현재 피로도가 최소 필요 피로도보다 크거나 같을 때만 던전 탐험 가능
-            if (currentFatigue >= minRequired) {
-                visited[i] = true;  // 던전 탐험 표시
-                dfs(currentFatigue - fatigueCost, exploredCount + 1, visited);  // 탐험 후 남은 피로도와 던전 탐험 수 업데이트
-                visited[i] = false;  // 탐험 취소 (다른 경로 탐색 위해)
+            
+            if(current >= min){
+                visited[i] = true;
+                dfs(current - cost, cnt+1, visited);
+                visited[i] = false;
             }
         }
+        
     }
-
-    // 방문 여부를 저장할 배열을 초기화하고 DFS 탐색 시작
-    dfs(k, 0, Array(dungeons.length).fill(false));
-
-    return maxDungeons;
+    
+    dfs(k, 0);
+    
+    return dungeonsCnt
+    
+      
+    
 }
