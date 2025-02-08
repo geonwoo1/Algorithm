81695b360eb2e34adb68ca2fb3c62fd553fce9f7
이건우 <90121680+geonwoo1@users.noreply.github.com>
2025-02-08 20:45:10 +0900

    [level 2] Title: 택배상자, Time: 30.12 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.js
+++ 프로그래머스/2/131704. 택배상자/택배상자.js
@@ -1,19 +1,18 @@
 function solution(order) {
-    var answer = 1;
-    let max = []
-    max[0] = order[0];
-    let sub = Array.from({ length: order[0] -1 }, (_, i) => i + 1);
-    for(var i = 1; i < order.length; i++){
-        if(max[max.length -1] < order[i]){
-            max.push(order[i])
-            answer+=1
-            for(var j = max[max.length - 2] +1; j < max[max.length -1]; j++){
-                sub.push(j);
-            }
-        }else{
-            if(sub.pop() == order[i]) answer+=1;
-            else return answer
-        }    
+    let answer = 0;
+    let stack = [];
+    let current = 1; 
+    for (let box of order) {
+        while (current <= box) {
+            stack.push(current++);
+
+        }
+        if (stack[stack.length - 1] === box) {
+            stack.pop();
+            answer++;
+        } else {
+            break; 
+        }
     }
     return answer;
 }
