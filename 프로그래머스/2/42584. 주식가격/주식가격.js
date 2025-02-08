ab82370315f07cd524db55b83f3903524020069e
이건우 <90121680+geonwoo1@users.noreply.github.com>
2025-02-08 18:11:25 +0900

    [level 2] Title: 주식가격, Time: 30.64 ms, Memory: 42.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.js
+++ 프로그래머스/2/42584. 주식가격/주식가격.js
@@ -1,20 +1,13 @@
 function solution(prices) {
-    const answer = Array(prices.length).fill(0); // 결과 배열 초기화
-    const stack = []; // 스택 초기화
+    let answer = Array.from({ length: prices.length }, (_, i) => prices.length - 1 - i);
+    let stack = [];
 
     for (let i = 0; i < prices.length; i++) {
-        // 스택의 마지막 값과 현재 값을 비교
         while (stack.length > 0 && prices[stack[stack.length - 1]] > prices[i]) {
-            const top = stack.pop(); 
-            answer[top] = i - top; // 시간 계산
+            let idx = stack.pop();
+            answer[idx] = i - idx;
         }
         stack.push(i);
-    }
-
-    // 스택에 남아있는 값 처리
-    while (stack.length > 0) {
-        const top = stack.pop();
-        answer[top] = prices.length - 1 - top;
     }
 
     return answer;
