a28ca7753ade26c0a923cd735f23ce3d23299469
이건우 <90121680+geonwoo1@users.noreply.github.com>
2025-02-19 19:51:49 +0900

    [level 2] Title: 무인도 여행, Time: 14.36 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
+++ 프로그래머스/2/154540. 무인도 여행/무인도 여행.js
@@ -1,36 +1,37 @@
 function solution(maps) {
-    const n = maps.length;
-    const m = maps[0].length;
-    const dx = [1, -1, 0, 0]; 
-    const dy = [0, 0, 1, -1];
-    const visited = Array.from({ length: n }, () => Array(m).fill(false));
+    var answer = [];
+    let n = maps.length;
+    let m = maps[0].length;
+    let dx = [1,0,-1,0];
+    let dy = [0,1,0,-1];
+    let visited = Array.from({length : n},()=>Array(m).fill(false));
     
     function bfs(startX, startY){
-        const queue = [[startX, startY]];
+        let que = [[startX, startY]];
         visited[startX][startY] = true;
-        let totalFood = parseInt(maps[startX][startY]);
-        
-        while(queue.length > 0){
-            const [x,y] = queue.shift();
-            for(var i = 0; i < 4; i++){
-                const nx = x + dx[i]
-                const ny = y + dy[i]
-                if(nx >= 0 && ny>=0 && nx<n && ny<m && !visited[nx][ny] && maps[nx][ny] !=='X'){
-                    visited[nx][ny] = true
-                    totalFood += parseInt(maps[nx][ny]);
-                    queue.push([nx, ny]);
+        let cnt = Number(maps[startX][startY]);
+        while(que.length > 0){
+            let [x,y]= que.shift();
+            for(var i =0; i<4; i++){
+                let nx = x+dx[i];
+                let ny = y+dy[i];
+                if(nx>=0 && ny>=0 &&nx<n &&ny<m &&!visited[nx][ny] && maps[nx][ny] !=='X'){
+                    visited[nx][ny] = true;
+                    que.push([nx,ny]);
+                    cnt+= Number(maps[nx][ny]);
                 }
             }
+            
         }
-        return totalFood;
+        return cnt;
     }
-    const answer = [];
-    for(var i = 0; i < n; i++){
-        for(var j = 0; j < m; j++){
-            if (maps[i][j] !== 'X' && !visited[i][j]) {
-                answer.push(bfs(i, j)); 
+    for(var i = 0; i<n; i++){
+        for(var j=0; j<m; j++){
+            if(maps[i][j] !='X' && !visited[i][j]){
+    
+                answer.push(bfs(i,j));
             }
         }
     }
-    return answer.length == 0 ? [-1] : answer.sort((a,b)=> a-b)
+    return answer.length === 0 ? [-1] : answer.sort((a, b) => a - b);
 }
